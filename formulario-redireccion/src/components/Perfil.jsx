0e53a2ad6ec792fd67060f0fbe5e66e6a8cf029d
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-15 12:52:24 -0600

    mostre los datos recibidos desde login

---
+++ formulario-redireccion/src/components/Perfil.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useLocation } from "react-router-dom";
+
+export default function Perfil() {
+    // Usamos useLocation para obtener todos los datos enviados desde el formulario de login
+    const location = useLocation();
+    const formData = location.state;
+
+    console.log("Datos recibidos del perfil:", formData)
+  return (
+    <div>
+        Bienvenido a tu perfil
+        <h1>{formData.name}{" "}{formData.apellido1} {" "}{formData.apellido2}</h1>
+        <h2>Edad: {formData.edad}</h2>
+        <div>   
+            <h3>Contacto:</h3> <br />
+            <p><strong>Correo:</strong> {formData.email} </p>
+            <p><strong>Telefono:</strong> {formData.telefono} </p>
+        </div>
+    </div>
+  )
+}
