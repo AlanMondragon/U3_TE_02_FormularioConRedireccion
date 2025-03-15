988ef91c01058cca007e5e1e922d86f51896e526
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2025-03-15 16:55:44 -0600

    import del css a perfil.jsx

--- formulario-redireccion/src/components/Perfil.jsx
+++ formulario-redireccion/src/components/Perfil.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import './Perfil.css'
 
 export default function Perfil() {
     // Usamos useLocation para obtener todos los datos enviados desde el formulario de login
