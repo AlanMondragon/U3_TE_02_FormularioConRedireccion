3db6fe5a6c2042426891899f75f536b7613da0ff
Salem <20233tn096@utez.edu.mx>
2025-03-15 08:55:53 -0600

    subo el Formulario de registro funcionando, y las rutas de navegación en el App

--- formulario-redireccion/src/App.jsx
+++ formulario-redireccion/src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Form from './components/Formulario'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
 
-  return (
-    <>
+  return(
+    <div>
       <Router>
-            <Routes>
-                <Route path="/"/>  
-            </Routes>
-        </Router>
-    </>
+        <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   )
+  
 }
 
 export default App
